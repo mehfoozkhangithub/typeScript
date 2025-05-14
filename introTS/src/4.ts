edf0269f314f1901d7673981255c050663277e2f
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-05-14 13:20:31 +0530

    code of the day

--- introTS/src/4.ts
+++ introTS/src/4.ts
@@ -38,8 +38,43 @@ let arr88: any[] = [1, 2, 3, "mehfooz", "khan"];
 /* interface and type -> create the blueprint/shape of  an object */
 
 type DATA = { id: number; name: string; address?: string }; // this is custome type
+/* interface DATA {
+  id: number;
+  name: string;
+  address?: string;
+} // this is custome type */
 
 let obj22: DATA = {
   id: 1,
   name: "mehfooz",
 };
+
+type Company = {
+  age?: number;
+  name?: string;
+  address?: string;
+};
+
+const masai: Company = {
+  name: "Masai",
+  address: "Bangalore",
+  age: 2,
+};
+
+const companies = [
+  {
+    name: "Masai",
+    address: "Bangalore",
+    age: 2,
+  },
+  {
+    name: "Flipcart",
+    address: "Pune",
+    age: 3,
+  },
+  {
+    name: "Zomato",
+    address: "Delhi",
+    age: 4,
+  },
+];
