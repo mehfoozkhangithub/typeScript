1bf38b136ab081d931f68ee30136fc720c3b9618
MehfoozKhan <khanmehfooz78603@gmail.com>
2025-06-03 15:39:57 +0530

    code of the day

---
+++ Advance_ts/src/1.ts
@@ -0,0 +1,21 @@
+// type Direction = 'east' | 'west' | 'south' | 'north';
+
+/*
+//! this is numaric enum
+
+enum Direction {
+  East,
+  Weast,
+  North,
+  South,
+}
+ */
+
+enum Direction {
+  East = 'east',
+  West = 'west',
+  North = 'north',
+  South = 'south',
+}
+
+let a: Direction = Direction.East;
--- introTS/src/4.ts
+++ introTS/src/4.ts
@@ -108,3 +108,62 @@ const masaiStudents: Company & SoftwareDeveloper = {
   age: 23,
   name: 'student_1',
 };
+
+//! use this way either
+/* type CartItems = {
+  id: number;
+  totalPrice: number;
+  product: { id: number; price: number; name: string; quantity: number }[];
+};
+ */
+
+type ProductType = {
+  id: number;
+  price: number;
+  name: string;
+  quantity: number;
+};
+
+type CartItems = {
+  id: number;
+  totalPrice: number;
+  products: ProductType[];
+};
+
+const cartItems: CartItems[] = [
+  {
+    id: 1,
+    products: [{ id: 1, name: 'Jacket', price: 300, quantity: 2 }],
+    totalPrice: 600,
+  },
+  {
+    id: 2,
+    products: [
+      { id: 1, name: 'Jacket', price: 300, quantity: 2 },
+      { id: 2, name: 'Sweater', price: 300, quantity: 2 },
+    ],
+    totalPrice: 1200,
+  },
+];
+
+// how we define the  value inside function
+
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+// console.log(sum('2', 3));
+
+/* 
+! that hy we give the number after function to get thr return value of type is number
+
+function sum(a: number, b: number) {
+  return `a + b`;
+}
+
+console.log(Math.floor(sum(2, 3)));
+ */
+
+const printData = ({ name, age, address }: Company): void => {
+  console.log(`${name} ${age} ${address}`);
+};
